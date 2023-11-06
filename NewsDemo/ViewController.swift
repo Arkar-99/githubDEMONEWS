2ba0da9cddb6f535247aafbdaac8c2c90ef50d09
Arkar-99 <koarkar.hml@gmail.com>
2023-11-06 16:00:10 +0630

    added project

---
+++ NewsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsDemo
+//
+//  Created by Arkar on 06/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+// testing
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
